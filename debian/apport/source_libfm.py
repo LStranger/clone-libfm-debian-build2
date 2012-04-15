d7339b4884c4a4330c1becb5215d333e6b36b2c6
Daniel Baumann <daniel@debian.org>
2012-04-15 19:18:30 +0000

    Removing whitespaces at EOL and EOF.

--- debian/apport/source_libfm.py
+++ debian/apport/source_libfm.py
@@ -1,16 +1,16 @@
 """
   Copyright (c) 2010 Julien Lavergne <gilir@ubuntu.com>
- 
+
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; either version 2 of the License, or
   (at your option) any later version.
- 
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
- 
+
   You should have received a copy of the GNU General Public License
   along with this program; if not, write to the Free Software Foundation,
   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
@@ -57,7 +57,7 @@ def add_info(report):
         report[report_pref_config_home] = apport.hookutils.read_file(os.path.join(home_conf_libfm,"pref-apps.conf"))
     else:
         report[report_pref_config_system] = apport.hookutils.read_file(os.path.join(system_conf_libfm,"pref-apps.conf"))
-    
+
     if os.path.exists(os.path.join(report_pcmanfm_home, conf_pcmanfm)):
         report[report_pcmanfm_home] = apport.hookutils.read_file(os.path.join(report_pcmanfm_home, conf_pcmanfm))
     else:
@@ -65,6 +65,6 @@ def add_info(report):
 
     # Attach information for relative packages
     apport.hookutils.attach_related_packages(report, ["libmenu-cache1","pcmanfm","pcmanfm2","udisks","gvfs","gvfs-backend"])
-    
+
     # Attach udisks info
     report["Udisks_dump"] = apport.hookutils.command_output(["udisks", "--dump"])
